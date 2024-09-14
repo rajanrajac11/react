596f693dd7a43af836632bd56ce2d43912d0504b
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-14 14:54:34 +0545

    passgeneratortest

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,84 +1,76 @@
-import { useState,useCallback, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef, useState } from "react"
 
-function App() {
-  let passwordRef = useRef(null)
+function App(){
+  const [length, setLength] = useState(8)
+  const [numAllow, setNumAllow] = useState(true)
+  const [charAllow, setCharAllow] = useState(true)
+  const [password, setPassword] = useState()
 
-  const copyPasswordToClipboard = ()=>{
-    passwordRef.current?.select()
-    passwordRef.current?.setSelectionRange(1,5)
-    
-    window.navigator.clipboard.writeText(password)
-  }
+  const passref = useRef(null)
 
-  const [length, setLength] = useState(10)
-  const [numAllow, setNumAllow] = useState(false)
-  const [charAllow, setCharAllow] = useState(false)
-  const [password, setPassword] = useState() 
-
-  const generatePassword = useCallback(()=> {
+  const generatePassword = useCallback(()=>{
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let pass = ""
-    if(numAllow) str += "1234567890"
-    if(charAllow) str += "!@#$%^&*(){}"
-    
-    for(let i =1; i<= length; i++){
+    if(charAllow)  str += "!@#$%^&*(){}"
+    if(numAllow) str+="1234567890"
+
+    for(let i = 1; i<=length; i++){
       pass += str.charAt(Math.floor(Math.random()*str.length))
-      
     }
     setPassword(pass)
-  }, [length, numAllow, charAllow, setPassword])
+  }, 
+[length, numAllow, charAllow,setPassword])
 
-useEffect(()=>{
-  generatePassword()
-},[length, numAllow, charAllow]
-)
+const copyToCLipBoard = useCallback(()=>{
+  passref.current?.select()
+  passref.current?.setSelectionRange(0,10)
+  window.navigator.clipboard.writeText(password)
+},[password])
 
-  return (
-    <div className='h-screen w-full bg-black flex justify-center items-center'>
-      <div className='flex flex-wrap bg-green-500'>
-        <div className='flex flex-wrap justify-center items-center p-3'>
-          <div className=''>
-          <h1 className='text-center '>Password Generator</h1>
-            <div className='p-3'>
-              <input type="text" name="" id=""
-              placeholder='Password'
-              value = {password}
-              className='w-80 p-1'
-              ref={passwordRef}
-              />
-              <button className='bg-blue-600 p-1' 
-              onClick={copyPasswordToClipboard}>Copy</button>
+useEffect(()=> {generatePassword()}, [length, numAllow, charAllow,generatePassword])
+  return(
+    <div className="h-screen w-full flex bg-blue-600 justify-center items-center">
+      <div className="bg-green-500 rounded-xl ">
+        <h2 className="text-center">Password Generator</h2>
+        <div className="p-2">
+          <div>
+            <input type="text" name="" id=""
+            placeholder="Password"
+            className="p-2 w-80"
+            value = {password} 
+            ref={passref}/>
+            <button className="bg-blue-600 p-2"
+            onClick={copyToCLipBoard}>Copy</button>
+          </div>
+
+          <div className="flex flex-wrap gap-3">
+            <div>
+              <input type="range" name="" id="" 
+              max = {90}
+              min = {6}
+              value={length}
+              onChange={(e)=> setLength(e.target.value)}/>
+              Length {length}
+            </div>
+            <div>
+              <input type="checkbox" name="" id="numAllow"
+              defaultChecked = {numAllow} 
+              onClick={()=> setNumAllow((prev)=> !prev) }/>
+              <label htmlFor="numAllow">Number</label>
             </div>
 
-            <div className='flex flex-wrap justify-center items-center gap-3 p-3'>
-              <div>
-                <input type="range" 
-                max={90}
-                min = {6}
-                value = {length}
-                onChange={(e)=> setLength(e.target.value)} />
-                <label htmlFor="">Length {length}</label>
-              </div>
-              <div>
-                <input type="checkbox" name="" id="numCheck"
-                defaultChecked = {numAllow}
-                onChange={()=> setNumAllow((prev)=> !prev)} />
-                <label htmlFor="numCheck">Numbers</label>
-              </div>
-
-              <div>
-                <input type="checkbox" name="" id="charCheck"
-                defaultChecked = {charAllow}
-                onChange={()=> setCharAllow((prev)=> !prev)} />
-                <label htmlFor="charCheck">Characters</label>
-              </div>
+            <div>
+              <input type="checkbox" name="" id="charAllow"
+              defaultChecked = {charAllow} 
+              onClick={()=> setCharAllow((prev)=> !prev) }/>
+              <label htmlFor="charAllow">Character</label>
             </div>
           </div>
+
         </div>
       </div>
     </div>
-  
   )
-}
 
+}
 export default App
