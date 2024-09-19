80985628ecd2d70d18c20b9b0b20a5bfa7611106
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-19 16:31:54 +0545

    mini context final test

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,25 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { ThemeContextProvider } from "./Context/Theme";
-import ThemeBtn from "./Component/ThemeBtn";
-import Card from "./Component/Card";
+import UserProvider from "./Context/UserProvider";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
-  function lightTheme() {
-    setThemeMode("light");
-  }
-  function darkTheme() {
-    setThemeMode("dark");
-  }
-  useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
   return (
-    <ThemeContextProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <ThemeBtn />
-      <Card />
-    </ThemeContextProvider>
+    <UserProvider>
+      <Login />
+      <Profile />
+    </UserProvider>
   );
 }
 
