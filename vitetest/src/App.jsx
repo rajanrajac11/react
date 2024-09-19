7b6919c637c008ff4173911ffc642c2807d9597b
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-19 16:47:23 +0545

    theme switcher final test

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from "react";
-import UserProvider from "./Context/UserProvider";
-import Login from "./Components/Login";
-import Profile from "./Components/Profile";
+
+import useTheme, { ThemeProvider } from "./Context/ThemeContext";
+import ThemeBtn from "./Components/ThemeBtn";
+import Card from "./Components/Card";
 
 function App() {
+  const [themeMode, setTheme] = useState("light");
+  function lightTheme() {
+    setTheme("light");
+  }
+  function darkTheme() {
+    setTheme("dark");
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <UserProvider>
-      <Login />
-      <Profile />
-    </UserProvider>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
