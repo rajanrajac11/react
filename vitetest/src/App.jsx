5051aa71275f7ae85ad52adc051a49517d7b2080
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-19 21:29:21 +0545

    todo test

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,37 +1,70 @@
 import React, { useEffect, useState } from "react";
-
-import useTheme, { ThemeProvider } from "./Context/ThemeContext";
-import ThemeBtn from "./Components/ThemeBtn";
-import Card from "./Components/Card";
+import { TodoProvider } from "./Context/TodoContext";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
 
 function App() {
-  const [themeMode, setTheme] = useState("light");
-  function lightTheme() {
-    setTheme("light");
-  }
-  function darkTheme() {
-    setTheme("dark");
-  }
+  const [todo, setTodo] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodo((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodo((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleCompleted = (id) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
 
   useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
+    const todo = JSON.parse(localStorage.getItem("todos"));
+    if (todo && todo.length > 0) {
+      setTodo(todo);
+    }
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todo));
+  }, [todo]);
   return (
-    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn />
+    <TodoProvider
+      value={{ todo, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
           </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
 
-          <div className="w-full max-w-sm mx-auto">
-            <Card />
+            {todo.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoList todo={todo} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </ThemeProvider>
+    </TodoProvider>
   );
 }
 
