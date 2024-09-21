3154185267fff6bf09e6b344eb40adfeec308bd7
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-21 23:05:47 +0545

    redux toolkit upto reducers

--- 11reduxToolkitTodo/src/features/Todo/todoSlice.js
+++ 11reduxToolkitTodo/src/features/Todo/todoSlice.js
@@ -1,34 +1,29 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit"
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id:1, text: "React with chai"}]
-
-
+    todos: [{
+        id: 1,
+        text: "React"
+    }]
 }
 
 export const todoSlice = createSlice({
-    name:'todo',
+    name: 'todo',
     initialState,
-    reducers: {
-        addTodo: (state, action)=>{
+    reducers:{
+        addTodo:(state, action)=>{
             const todo = {
-                id: nanoid(),
-                text: action.payload
+                id:nanoid(),
+                text:action.payload.text
             }
             state.todos.push(todo)
         },
-        removeTodo:(state, action )=>{
-            state.todos = state.todos.filter((prev)=>prev.id!=action.payload)
-
+        removeTodo:(state, action)=>{
+           state.todos =  state.todos.filter((todo)=> action.payload.id!=todo.id)
         },
+
         updateTodo:(state, action)=>{
-            state.todos = state.todos.map((prev)=>(prev.id===action.payload.id)? prev.text=action.payload.text:prev.text)
-
+           state.todos =  state.todos.map((todo)=> todo.id==action.payload.id? {...todo, text:action.payload.text}: todo)
         }
-
     }
 })
-
-export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
-
-export default todoSlice.reducer
