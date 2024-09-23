d439463327ebbcd9aa9b0f8d202def56492ac23d
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-23 22:34:56 +0545

    counter rev

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
-
-import { CounterProvider } from "./Context/CounterContext";
-import Counter from "./Components/Counter";
+import Counter from "./assets/Components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <CounterProvider value={{ count, setCount }}>
+    <>
       <Counter />
-    </CounterProvider>
+    </>
   );
 }
 
