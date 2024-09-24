5750effc47b065b9e79cd3f2148f724c87a31c1d
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-24 22:51:44 +0545

    miniUserContext rev

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,7 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import UserProvider from "./Context/UserProvider";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
 
 function App() {
-  return <></>;
+  const [user, setUser] = useState("");
+  return (
+    <UserProvider values={{ user, setUser }}>
+      <Login />
+      <Profile />
+    </UserProvider>
+  );
 }
 
 export default App;
--- vitetest/src/main.jsx
+++ vitetest/src/main.jsx
@@ -2,40 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Layout from "./Layout";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import User from "./Components/User";
-import Github, { githubInfo } from "./Components/Github";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "users/:id",
-        element: <User />,
-      },
-      {
-        path: "github",
-        element: <Github />,
-        loader: () => githubInfo(),
-      },
-    ],
-  },
-]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
