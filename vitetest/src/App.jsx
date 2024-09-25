78e28c44c5d722bf652ecdb0050f30c4087c2c32
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-25 22:34:29 +0545

    themeSwitcher test

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
-import UserProvider from "./Context/UserProvider";
-import Login from "./Components/Login";
-import Profile from "./Components/Profile";
+import React, { useEffect, useState } from "react";
+import useTheme, { ThemeProvider } from "./Context/ContextProvider";
+import Card from "./Components/Card";
+import ThemeBtn from "./Components/ThemeBtn";
 
 function App() {
-  const [user, setUser] = useState("");
+  const [themeMode, setTheme] = useState("light");
+  function lightTheme() {
+    setTheme("light");
+  }
+  function darkTheme() {
+    setTheme("dark");
+  }
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <UserProvider values={{ user, setUser }}>
-      <Login />
-      <Profile />
-    </UserProvider>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
