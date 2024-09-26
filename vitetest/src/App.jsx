72706080c5d62102deaddfd917a724da23797955
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-26 11:35:22 +0545

    react-redux rev

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,36 +1,13 @@
 import React, { useEffect, useState } from "react";
-import useTheme, { ThemeProvider } from "./Context/ContextProvider";
-import Card from "./Components/Card";
-import ThemeBtn from "./Components/ThemeBtn";
+import AddTodo from "./Components/AddTodo";
+import Todos from "./Components/Todos";
 
 function App() {
-  const [themeMode, setTheme] = useState("light");
-  function lightTheme() {
-    setTheme("light");
-  }
-  function darkTheme() {
-    setTheme("dark");
-  }
-
-  useEffect(() => {
-    document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
-
   return (
-    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn />
-          </div>
-
-          <div className="w-full max-w-sm mx-auto">
-            <Card />
-          </div>
-        </div>
-      </div>
-    </ThemeProvider>
+    <>
+      <AddTodo />
+      <Todos />
+    </>
   );
 }
 
---
+++ vitetest/src/Features/Todo/TodoSlice.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {createSlice, nanoid} from "@reduxjs/toolkit"
+
+const initialState ={
+    todos:[
+        {
+            id:1,
+            text:'React Redux'
+        }
+    ]
+}
+
+export const TodoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state, action)=>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state, action)=>{
+            state.todos = state.todos.filter((todo)=> todo.id!==action.payload)
+        }
+    }
+
+})
+
+export const {addTodo, removeTodo} = TodoSlice.actions
+
+export default TodoSlice.reducer
