8716a2f8b9bc169cd9a0c4b676f0f7eb89a1dfc0
Rajan <acharya.rajan.raj11@gmail.com>
2024-09-29 22:30:07 +0545

    create account err msg

--- 12MegaProject/src/appwrite/auth.js
+++ 12MegaProject/src/appwrite/auth.js
@@ -28,7 +28,7 @@ export class AuthService{
            }
         }
         catch(error){
-            throw error;
+            console.log("Appwrite service :: createAccount :: error", error)
         }
     }
 
