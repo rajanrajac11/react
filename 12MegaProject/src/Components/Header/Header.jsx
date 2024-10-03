2219f37ce5a05ab50df4bf1dfffbb831f6c7bb8c
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-03 21:40:24 +0545

    Header component

--- 12MegaProject/src/Components/Header/Header.jsx
+++ 12MegaProject/src/Components/Header/Header.jsx
@@ -1,2 +1,44 @@
 import React from "react";
 import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import React from "react";
+
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      path: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      path: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      path: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      path: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      path: "/add-post",
+      active: authStatus,
+    },
+  ];
+
+  return <div>Header</div>;
+}
+
+export default Header;
