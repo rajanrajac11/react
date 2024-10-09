98403631f76b5a918086b5fc29940c572560d273
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-09 17:23:08 +0545

    components rev

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
-import { Footer, Header } from "./Components";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
+
   useEffect(() => {
     authService
       .getCurrentUser()
@@ -21,11 +24,12 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
-
-        <main></main>
+        <main>
+          TODO: <Outlet />
+        </main>
         <Footer />
       </div>
     </div>
