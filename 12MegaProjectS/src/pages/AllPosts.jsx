00717e822e037197665748fa52b76a2a606cf5af
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-25 21:10:55 +0545

    bug

--- 12MegaProjectS/src/pages/AllPosts.jsx
+++ 12MegaProjectS/src/pages/AllPosts.jsx
@@ -15,7 +15,7 @@ function AllPosts() {
     <div className="w-full py-8">
       <Container>
         <div className="flex flex-wrap">
-          {posts?.map((post) => (
+          {posts.map((post) => (
             <div key={post.$id} className="py-2 w-1/4">
               <PostCard {...post} />
             </div>
