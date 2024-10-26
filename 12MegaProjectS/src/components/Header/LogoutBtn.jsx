d743ed765b69cf7a80cd33834f8f7bba4f693ae9
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-26 12:08:24 +0545

    logout navigation improvement

--- 12MegaProjectS/src/components/Header/LogoutBtn.jsx
+++ 12MegaProjectS/src/components/Header/LogoutBtn.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { logout } from "../../store/AuthSlice";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn(width = "100px") {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const logoutHandler = () => {
     authService.logout().then(() => [dispatch(logout())]);
+    navigate("/");
   };
   return (
     <button width={width} onClick={logoutHandler}>
