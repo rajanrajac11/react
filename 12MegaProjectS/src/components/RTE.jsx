5e4e4629c145db427555fc197ec6841bd096f966
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-26 11:16:06 +0545

    RTE typo fix

--- 12MegaProjectS/src/components/RTE.jsx
+++ 12MegaProjectS/src/components/RTE.jsx
@@ -45,7 +45,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
-            onEditorChange={onchange}
+            onEditorChange={onChange}
           />
         )}
       />
