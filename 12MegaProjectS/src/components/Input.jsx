979f1838bece0813e25eee6c9f06bbab9f6c62c9
Rajan <acharya.rajan.raj11@gmail.com>
2024-10-28 08:43:07 +0545

    css

--- 12MegaProjectS/src/components/Input.jsx
+++ 12MegaProjectS/src/components/Input.jsx
@@ -15,7 +15,7 @@ function Input(
       )}
       <input
         type={type}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-500 duration-200 border border-gray-200 w-full ${className}`}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-200 duration-200 border border-gray-200 w-full ${className}`}
         name=""
         id={id}
         placeholder={placeholder}
--- 12MegaProjectS/src/components/Login.jsx
+++ 12MegaProjectS/src/components/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
         <div className="mb-2 flex justify-center">
-          <span className="inline-block w-full max-w-[100px]">
+          <span className="inline-block w-full max-w-[100px] bg-gray-600 rounded-xl">
             <Logo width="100%" />
           </span>
         </div>
