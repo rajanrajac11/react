22e43eea5c3a39401eedf69e6b8024b2413a00e9
Rajan <acharya.rajan.raj11@gmail.com>
2024-11-22 21:58:56 +0545

    race condition solved

--- APIHandling/Frontend/src/App.jsx
+++ APIHandling/Frontend/src/App.jsx
@@ -5,31 +5,67 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  // const { products, error, loading } = customReactQuery("/api/products");
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
     (async () => {
       try {
+        setLoading(true);
         setError(false);
-        const response = await axios.get("/api/products");
+        const response = await axios.get("/api/products?search=" + search, {
+          signal: controller.signal,
+        });
         setProducts(response.data);
         console.log(response.data);
+        setLoading(false);
       } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request Cancelled", error.message);
+          return;
+        }
         setError(true);
+        setLoading(false);
+        console.log(error);
       }
     })();
-  }, []);
 
-  if (error) {
-    return <h1>Something went wrong</h1>;
-  }
+    //cleanupcode
+    return () => {
+      controller.abort();
+    };
+  }, [search]);
+
   return (
     <>
       <h1>API handling</h1>
+      <input
+        type="text"
+        name=""
+        placeholder="Search"
+        id=""
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Someting went wrong</h1>}
       <h2>Total no. of products : {products.length}</h2>
     </>
   );
 }
 
 export default App;
+
+const customReactQuery = (urlPath) => {
+  return {
+    products,
+    error,
+    loading,
+  };
+};
